d2a3cf4c577fec4930b4962984dfab4c1bd05cb6
gaojunran <nebula2021@126.com>
2024-06-28 17:39:37 +0800

    03

--- vmaker/main.py
+++ vmaker/main.py
@@ -1,19 +1,32 @@
+import os
+import sys
 from pathlib import Path
 from typing import Annotated
 
 import typer
 import rich
+from rich.console import Console
 from rich.prompt import Prompt
 
 from vmaker.constants import Actions
-from vmaker.utils import print_videos_info, get_latest_video
+from vmaker.utils import print_videos_info, get_latest_video, env_init
 
 app = typer.Typer()
 
-SOURCE_DIR = r"C:\Users\nebul\Videos"
-SOURCE_PATH = Path(SOURCE_DIR)
-DEST_DIR = r"D:\_WORKS\_video\01"
-DEST_PATH = Path(DEST_DIR)
+RAW_DIR = os.getenv("VMAKER_RAW_DIR")
+RAW_PATH = RAW_DIR and Path(RAW_DIR)
+CLIP_DIR = os.getenv("VMAKER_CLIP_DIR")
+CLIP_PATH = CLIP_DIR and Path(CLIP_DIR)
+OUTPUT_DIR = os.getenv("VMAKER_OUTPUT_DIR")
+OUTPUT_PATH = OUTPUT_DIR and Path(OUTPUT_DIR)
+
+CWD = Path.cwd()
+
+def throw_if_not_init():
+	if not (RAW_DIR and CLIP_DIR and OUTPUT_DIR):
+		err_console = Console(stderr=True, style="bold red")
+		err_console.print("Please run `vmaker init` first.")
+		sys.exit()
 
 
 @app.callback()
@@ -23,26 +36,53 @@ def callback():
     """
 	pass
 
+@app.command()
+def init():
+	"""
+	Config default settings for the first time.
+	If you want one of them to be changed, run `vmaker config [...]` instead.
+	"""
+	raw_dir = os.getenv("VMAKER_RAW_DIR") or Prompt.ask("The path where videos are recorded")
+	clip_dir = os.getenv("VMAKER_CLIP_DIR") or Prompt.ask("The path where clips are saved", default=f"{CWD / "clips"}")
+	output_dir = os.getenv("VMAKER_OUTPUT_DIR") or Prompt.ask("The path where final videos are saved", default=f"{CWD / "output"}")
+	env_init(raw_dir, clip_dir, output_dir)
+	typer.echo("Success! If it still doesn't work well, please reboot the terminal.")
+
+
+def config():
+	pass
 
 @app.command()
 def add(
 		new_name: Annotated[str, typer.Argument(help="The new name of the video.")] = Actions.DONT_RENAME,
+		choose: Annotated[bool, typer.Option("--choose", "-c", help="Choose the video to add.")] = False,
 ):
 	"""
-	Add the latest recorded video to the predetermined folder.
+	Add the latest recorded video to the clip folder.
 	"""
-	video = get_latest_video(SOURCE_PATH)
+	throw_if_not_init()
+	if not choose:
+		video = get_latest_video(RAW_PATH)
+	else:
+		choices = get_latest_video(RAW_PATH, 6)
+		print_videos_info(choices)
+		index = Prompt.ask("Choose one from above: ", choices=["0", "1", "2", "3", "4", "5"], default="0")
+		video = choices[int(index)]
 	suffix = video.suffix
-	rich.print(f"Will copy the video below to [green]{DEST_DIR}[/green] with the new name [green]{new_name}{suffix}[/green]. ")
+	rich.print(
+		f"Will copy the video below to [green]{CLIP_DIR}[/green] with the new name [green]{new_name}{suffix}[/green]. ")
 	print_videos_info([video])
 	choice = Prompt.ask("Sure to continue?", choices=["Y", "n"], default="Y")
 	if choice == "Y":
 		print("Success!")
+
 
 @app.command()
 def dir():
 	"""
 	Show source directory, destination directory and video output directory.
 	"""
-	rich.print(f"[green]Source Dir[/green]: [link=file:///{SOURCE_DIR}]{SOURCE_DIR}[/link]")
-	rich.print(f"[green]Destination Dir[/green]: [link=file:///{DEST_DIR}]{DEST_DIR}[/link]")
+	throw_if_not_init()
+	rich.print(f"[green]RAW Dir[/green]: [link=file:///{RAW_DIR}]{RAW_DIR}[/link]")
+	rich.print(f"[green]CLIP Dir[/green]: [link=file:///{CLIP_DIR}]{CLIP_DIR}[/link]")
+	rich.print(f"[green]OUTPUT Dir[/green]: [link=file:///{OUTPUT_DIR}]{OUTPUT_DIR}[/link]")
--- vmaker/utils.py
+++ vmaker/utils.py
@@ -1,3 +1,7 @@
+import json
+import os
+import platform
+import subprocess
 from pathlib import Path
 from datetime import datetime
 import rich
@@ -5,15 +9,39 @@ from rich.console import Console
 from rich.table import Table
 
 
+
+def env_init(raw_dir, clip_dir, output_dir):
+	_set_env_var("VMAKER_RAW_DIR", raw_dir)
+	_set_env_var("VMAKER_CLIP_DIR", clip_dir)
+	_set_env_var("VMAKER_OUTPUT_DIR", output_dir)
+
+
+def _set_env_var(name, value):
+	system = platform.system()
+	if system == 'Windows':
+		# 设置Windows环境变量
+		subprocess.run(['setx', name, value], shell=True)
+	elif system in ('Linux', 'Darwin'):  # Darwin表示macOS
+		# 设置Linux或macOS环境变量
+		shell_config_file = os.path.expanduser('~/.bashrc')
+		with open(shell_config_file, 'a') as file:
+			file.write(f'\nexport {name}={value}\n')
+		print(f'Run `source {shell_config_file}` to make the changes effective.')
+	else:
+		raise NotImplementedError(f'Unsupported platform: {system}')
+
+
 def print_videos_info(videos: list[Path]):
 	table = Table()
+	table.add_column("Idx", justify="right", style="dim")
 	table.add_column("Name", justify="right", style="cyan", no_wrap=True)
 	table.add_column("Datetime", justify="right", style="magenta")
 	table.add_column("Length(H:S:M)", justify="right", style="green")
 	table.add_column("Size(MB)", justify="right", style="green")
-	for video in videos:
+	for idx, video in enumerate(videos):
 		video: Path
 		table.add_row(
+			str(idx),
 			video.name,
 			datetime.fromtimestamp(video.stat().st_mtime).strftime("%Y-%m-%d %H:%M:%S"),
 			"TODO",
@@ -22,7 +50,8 @@ def print_videos_info(videos: list[Path]):
 	console = Console()
 	console.print(table)
 
-def get_latest_video(dir: Path):
+
+def get_latest_video(dir: Path, num=1):
 	all_files = list(dir.glob('**/*'))
 	video_files = [file for file in all_files if file.suffix in ['.mp4', '.mkv']]
-	return max(video_files, key=lambda file: file.stat().st_mtime)
+	return sorted(video_files, key=lambda file: file.stat().st_mtime)[:num]
